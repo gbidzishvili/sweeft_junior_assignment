fe908a05056f1941e68891f5b76d916569cfc9dc
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-01 13:09:13 +0400

    removed unused files

--- src/app/features/recipes/views/home-pg/home-pg.module.ts
+++ src/app/features/recipes/views/home-pg/home-pg.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePgRoutingModule } from './home-pg-routing.module';
 import { MaterialImportsModule } from './material-imports.module';
-import { RecipeListContainerComponent } from './recipe-list-container/recipe-list-container.component';
-import { RecipeCardComponent } from './recipe-list-container/recipe-card/recipe-card.component';
+import { RecipeListContainerComponent } from './components/recipe-list-container/recipe-list-container.component';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [RecipeListContainerComponent, RecipeCardComponent],
