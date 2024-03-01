3a976aaefec2c406c10090ccba4d912c8c00e590
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-01 13:17:09 +0400

    recipe-details added

--- src/app/features/recipes/views/home-pg/home-pg-routing.module.ts
+++ src/app/features/recipes/views/home-pg/home-pg-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePgComponent } from './home-pg.component';
+import { RecipeListContainerComponent } from './components/recipe-list-container/recipe-list-container.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePgComponent },
+  {
+    path: 'home',
+    component: HomePgComponent,
+    children: [
+      { path: '', redirectTo: 'recipe-list', pathMatch: 'full' },
+      { path: 'recipe-list', component: RecipeListContainerComponent },
+      { path: 'details', component: RecipeDetailsComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/features/recipes/views/home-pg/home-pg.module.ts
+++ src/app/features/recipes/views/home-pg/home-pg.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePgRoutingModule } from './home-pg-routing.module';
 import { MaterialImportsModule } from './material-imports.module';
 import { RecipeListContainerComponent } from './components/recipe-list-container/recipe-list-container.component';
-import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
+import { RecipeCardComponent } from './components/recipe-list-container/recipe-card/recipe-card.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 @NgModule({
-  declarations: [RecipeListContainerComponent, RecipeCardComponent],
+  declarations: [
+    RecipeListContainerComponent,
+    RecipeCardComponent,
+    RecipeDetailsComponent,
+  ],
   imports: [CommonModule, HomePgRoutingModule, MaterialImportsModule],
   exports: [RecipeListContainerComponent],
 })
