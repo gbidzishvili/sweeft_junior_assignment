97f2c1c3353e76f4243e1c99e3f8f93e69711f64
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-03 18:39:55 +0400

    recipe form validation completed

--- src/app/features/recipes/views/home-pg/components/recipe-form/recipe-form.component.ts
+++ src/app/features/recipes/views/home-pg/components/recipe-form/recipe-form.component.ts
@@ -8,8 +8,9 @@ import {
   Validators,
 } from '@angular/forms';
 import { RecipeServiceService } from '../../../../services/recipe-service.service';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { ImgFileUploadService } from './services/img-file-upload.service';
 
 @Component({
   selector: 'app-recipe-form',
@@ -17,42 +18,49 @@ import { Router } from '@angular/router';
   styleUrl: './recipe-form.component.scss',
 })
 export class RecipeFormComponent implements OnInit, OnDestroy {
-  fileName!: string;
+  fileName$: BehaviorSubject<string> = new BehaviorSubject('');
+  baseString$: BehaviorSubject<string> = new BehaviorSubject('');
   recipeForm!: FormGroup;
   formSubscription!: Subscription;
   errors = ['required', 'maxlength', 'pattern'];
   errorMessages: { [key: string]: string } = {
     required: '* This field is required',
     maxlength: '* Text cannot exceed 20 characters',
-    pattern: '* Use only English letters',
+    // pattern: '* Use only English letters',
   };
   constructor(
     private fb: FormBuilder,
-    private recipeService: RecipeServiceService
+    private recipeService: RecipeServiceService,
+    private imgFileUploadService: ImgFileUploadService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.recipeForm = this.fb.group({
-      image: [null, Validators.required],
       name: [null, [Validators.required, Validators.maxLength(20)]],
       description: [null, Validators.required],
-      vegie: [false],
+      image: [null, Validators.required],
       ingredients: this.fb.array([new FormControl(null, Validators.required)]),
       directions: this.fb.array([new FormControl(null, Validators.required)]),
+      vegie: [false],
     });
+    this.fileName$ = this.imgFileUploadService.FileNameSrv$;
+    this.imgFileUploadService.BaseStringSrv$.subscribe((baseStr) =>
+      this.recipeForm.get('image')?.setValue(baseStr)
+    );
   }
-  // managing dynamic form
+  // dynamically adding or removing ingredients and directions
   getIngredients(): FormArray {
     return this.recipeForm.get('ingredients') as FormArray;
   }
   getNewIngredient(): FormControl {
-    return new FormControl();
+    return new FormControl(null, Validators.required);
   }
   getDirections(): FormArray {
     return this.recipeForm.get('directions') as FormArray;
   }
   getNewDirection(): FormControl {
-    return new FormControl();
+    return new FormControl(null, Validators.required);
   }
   addNewIngredientBtnClidk(): void {
     this.getIngredients().push(this.getNewIngredient());
@@ -66,21 +74,12 @@ export class RecipeFormComponent implements OnInit, OnDestroy {
   removeDirectionBtnClick(directionIndex: number) {
     this.getDirections().removeAt(directionIndex);
   }
-  // file upload as base64 string
-  onFileSelected(event: Event) {
-    const fileInputElement = event.target as HTMLInputElement;
-    if (fileInputElement.files?.[0]) {
-      this.fileName = fileInputElement.files?.[0].name;
-      var reader = new FileReader();
-      reader.onloadend = () => {
-        var baseStringResut = reader.result as string;
-        this?.recipeForm.get?.('image')?.setValue(baseStringResut);
-        console.log(this.recipeForm.get('image'));
-      };
-      reader.readAsDataURL(fileInputElement.files[0]);
-    }
+  // use service to upload image
+  onFileSelected(event: Event): void {
+    this.imgFileUploadService.uploadImage(event.target as HTMLInputElement);
   }
-  checkIfHasError(control: string, error: string): boolean | undefined {
+  // display error on invalid input
+  checkIfInputHasError(control: string, error: string): boolean | undefined {
     return (
       this.recipeForm.get(control)?.touched &&
       this.recipeForm.get(control)?.hasError(error)
@@ -88,12 +87,19 @@ export class RecipeFormComponent implements OnInit, OnDestroy {
   }
 
   onFormsubmit() {
+    this.recipeForm.markAllAsTouched();
     console.log(this.recipeForm.valid);
-    this.formSubscription = this.recipeService
-      .createRecipe(this.recipeForm.value)
-      .subscribe();
-    // this.router.navigate(['/home', 'list']);
-    console.log(this.recipeForm);
+    if (this.recipeForm.valid) {
+      this.formSubscription = this.recipeService
+        .createRecipe(this.recipeForm.value)
+        .subscribe();
+      this.imgFileUploadService.FileNameSrv$.next('No image uploaded yet.');
+      this.imgFileUploadService.BaseStringSrv$.next('');
+      this.router.navigate(['/home', 'list']);
+
+      return;
+    }
+    this.fileName$.next('* File upload is required');
   }
 
   ngOnDestroy() {
